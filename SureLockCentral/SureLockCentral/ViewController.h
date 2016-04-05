bf4873bb14059adbce893b66fc1ab039c1855fc9
atitus5 <drewtitus59@gmail.com>
2016-04-04 20:01:32 -0400

    Started central app

---
+++ SureLockCentral/SureLockCentral/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SureLockCentral
+//
+//  Created by Andrew Titus on 4/4/16.
+//  Copyright © 2016 Drew Titus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SureLockCentral/SureLockCentral/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SureLockCentral
+//
+//  Created by Andrew Titus on 4/4/16.
+//  Copyright © 2016 Drew Titus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
